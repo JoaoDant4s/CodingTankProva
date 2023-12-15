e2ce822f4cfa2ae2733e059018bc54605f65ebb5
João Dantas <JoaoDant4s>
2023-12-15 20:00:27 -0300

    first question done

--- Q1/src/Main.java
+++ Q1/src/Main.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void printResults(int tempoEmMeses, double amortizacao, double saldoDevedorAtual, double taxaJuros, double saldoDevedorTotal){
+        double juros_mensal;
+        double parcela_mensal;
+        double totalPagoDeJuros = 0;
+        System.out.printf("Valor fixo da amortização R$ %.2f, Saldo devedor total R$: %.2f com um juros de %.2f%% ao mês\n", amortizacao, saldoDevedorTotal, taxaJuros);
+        for(int i = 1; i <= tempoEmMeses; i++){
+            juros_mensal = (saldoDevedorAtual * taxaJuros) / 100;
+            totalPagoDeJuros += juros_mensal;
+            saldoDevedorAtual -= amortizacao;
+            parcela_mensal = juros_mensal + amortizacao;
+            System.out.printf("Parcela %d | Juros: R$ %.2f | Prestação: R$ %.2f | Saldo devedor: R$ %.2f\n", i, juros_mensal, parcela_mensal, saldoDevedorAtual);
         }
+        System.out.printf("Total: Prestação R$ %.2f, Juros R$ %.2f, Amortização R$ %.2f", (saldoDevedorTotal + totalPagoDeJuros), totalPagoDeJuros, saldoDevedorTotal);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o valor do empréstimo: ");
+        double saldoDevedorTotal = scanner.nextFloat();
+        System.out.print("Informe a taxa de juros: ");
+        double taxaJuros = scanner.nextFloat();
+        System.out.print("Em quanto tempo quer pagar? ");
+        int tempoEmMeses = scanner.nextInt();
+
+
+        double amortizacao = saldoDevedorTotal / tempoEmMeses;
+        double saldoDevedorAtual = saldoDevedorTotal;
+        printResults(tempoEmMeses, amortizacao, saldoDevedorAtual, taxaJuros, saldoDevedorTotal);
     }
 }
