9eac4b8815bac67567a87560037570ffa87276a2
João Dantas <JoaoDant4s>
2023-12-15 21:05:19 -0300

    third question done

---
+++ Q3/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static void desenhar(int inteiro){
+        for (int i = 1; i <= (inteiro / 2) + 1; i++) {
+            for (int j = 0; j < inteiro - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int inteiro = scanner.nextInt();
+
+        desenhar(inteiro);
+    }
+}
