24f98f5520f106da69d9f06b6a0afb574b05fa63
João Dantas <JoaoDant4s>
2023-12-15 21:10:05 -0300

    extra question done

--- Q3/src/Main.java
+++ Q3/src/Main.java
@@ -13,6 +13,15 @@ public class Main {
             }
             System.out.print("\n");
         }
+        for (int i = (inteiro / 2) + 1; i > 0; i--) {
+            for (int j = 0; j < inteiro - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 
     public static void main(String[] args) {
